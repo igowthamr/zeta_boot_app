304d69d3d47e2dbfe078fa72b7f1268a85de469b
igowthamr <igowthamr@gmail.com>
2020-10-25 15:12:51 +0530

    Adding stubs

---
+++ src/main/java/com/example/demo/ZetaBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetaBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZetaBootAppApplication.class, args);
+	}
+
+}
